1ef188cb096f5f4870b7e9dd3e5b5e3960e1f022
milospetrovic <milosp@ultimatemovers.net>
2022-06-02 16:17:02 +0000

    update services categories icons

--- resources/views/web/pages/services.blade.php
+++ resources/views/web/pages/services.blade.php
@@ -5,26 +5,24 @@
 @section('content')
   <section>
     <div class="container mx-auto pt-20">
-      <h2 class="text-2xl font-medium tracking-wide w-1/2 leading-normal">{{ __('header.services') }}</h2>
+      <h2 class="text-6xl font-medium tracking-wide font-lora">{{ __('header.services') }}</h2>
     </div>
   </section>
 
   <section>
     <div class="container mx-auto pb-10">
-      <div class="w-full grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 mt-20">
-        @foreach(App\Models\Category::where('parent_id', 0)->get() as $category)
-        <article class="single-article-product">
-          <a href="/{{ $category->slug }}" class="block cursor-pointer order-{{ $category->order }}">
-            <div id="single-product-bigimage" class="w-full max-h-290 aspect-w-1 aspect-h-1 single-product-gallery--big rounded-md border border-gray-100 shadow-sm">
-              <div class="h-full bg-cover bg-center bg-no-repeat rounded-md single-product-bigimage-url" data-zoom="/{{ isset($category->image) ? $category->image->file_path : 'images/product-placeholder.png' }}" style="background-image: url('/{{ isset($category->image) ? $category->image->file_path : 'images/product-placeholder.png' }}')"></div>
-            </div>
-            <div class="text-xl font-medium text-center mt-3 text-gray-800 tracking-wide leading-normal">
-              {{ $category->translatedName }}
-            </div>
-          </a>
-        </article>
-        @endforeach
-      </div>
+      <div class="w-full grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 mt-32">
+          @foreach(App\Models\Category::where('parent_id', 0)->get() as $category)
+            <a href="/{{ $category->slug }}" class="block single-article-product cursor-pointer order-{{ $category->order }}">
+              <div class="w-2/3 mx-auto">
+                <img src="/images/categories/{{ $category->slug }}.svg" alt="{{ $category->slug }}" width="100%" />
+              </div>
+              <div class="text-xl font-medium text-center mt-3 text-gray-800 tracking-wide leading-normal">
+                {{ $category->translatedName }}
+              </div>
+            </a>
+          @endforeach
+        </div>
     </div>
   </section>
 
