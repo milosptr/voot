6a7288e3c08529cbca00d8cbd2e870ff4dabc747
milospetrovic <milosp@ultimatemovers.net>
2022-06-07 15:22:47 +0000

    http to https

---
+++ app/Services/LisaAxService.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Services;
+
+use App\Models\User;
+
+class LisaAxService {
+  public $user;
+  public $requestURL;
+  public $requestType;
+  public $body;
+
+  public function __construct()
+  {
+    $this->requestURL = 'https://213.167.137.207:1456/LisaAxServices.asmx';
+    $this->requestType = 'POST';
+
+    return $this;
+  }
+
+  public static function forCustomer(User $user)
+  {
+    return new Static($user);
+  }
+
+  public function setRequestURL($url)
+  {
+    $this->requestURL = $url;
+    return $this;
+  }
+
+  public function setRequestType($requestType)
+  {
+    $this->requestType = $requestType;
+  }
+
+  public function setRequestBody($body) {
+    $this->body = $body;
+    return $this;
+  }
+
+  public function send() {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $this->requestURL,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $this->requestType,
+      CURLOPT_POSTFIELDS => $this->body,
+      CURLOPT_HTTPHEADER => array(
+        'Content-Type: text/xml'
+      ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $response;
+  }
+}
