83377ab3aa0ae39fdf859902ab8a1aaa1675a0ec
milospetrovic <milosp@ultimatemovers.net>
2022-06-15 16:15:17 +0000

    status == ax_status

--- resources/views/components/order/ax-statuses.blade.php
+++ resources/views/components/order/ax-statuses.blade.php
@@ -1,4 +1,4 @@
-@if($status === App\Models\Order::AX_STATUS_SAVED)
+@if($status == App\Models\Order::AX_STATUS_SAVED)
 <div class="order-status-box w-20 text-center mt-1 inline-block shadow-sm text-sm sm:text-xs rounded-md uppercase font-bold tracking-wide bg-green-200 text-green-500 border-1 border-green-200 py-1 px-2 mb-1 ">
   SAVED
 </div>
