e18cbbbb7667e0fccf62f1198dc70847e4aee31e
milospetrovic <milosp@ultimatemovers.net>
2022-06-18 15:14:37 +0000

    update logo height in emails

--- resources/views/common/email/header.blade.php
+++ resources/views/common/email/header.blade.php
@@ -1,7 +1,7 @@
 <tr valign="center">
   <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; text-align:right;" valign="center">
-    <div style="background: rgb(16, 37, 58);padding: 12px; display:inline-block; margin-bottom: 10px;">
-      <img class="h-12 w-auto" src="{{ env('APP_URL') }}/images/voot-logo.png" alt="Voot">
+    <div style="background: rgb(16, 37, 58);padding: 8px 12px; display:inline-block; margin-bottom: 10px;">
+      <img src="{{ env('APP_URL') }}/images/voot-logo.png" alt="Voot" height="38">
     </div>
   </td>
 </tr>
