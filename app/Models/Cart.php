a0965b04adaf24f14f54f06405c625b5eeeb896f
milospetrovic <milosp@ultimatemovers.net>
2022-12-10 16:49:36 +0000

    update cart number function

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -20,7 +20,7 @@ class Cart extends Model
 
     public static function cartNumber($id) {
       $cart =  Cart::where('user_id', $id)->first();
-      if($cart)
+      if($cart && json_decode($cart->cart))
         return count(json_decode($cart->cart));
       return 0;
     }
--- resources/views/web/navigation.blade.php
+++ resources/views/web/navigation.blade.php
@@ -63,12 +63,12 @@
         </a>
         <a href="/cart" class="ml-4 text-gray-600 hover:text-primary-light relative" aria-label="Cart" >
           @if(auth()->user())
-            {{-- @php $inCart = App\Models\Cart::cartNumber(auth()->user()->id); @endphp --}}
-            {{-- @if($inCart)
+            @php $inCart = App\Models\Cart::cartNumber(auth()->user()->id); @endphp
+            @if($inCart)
               <div class="absolute right-0 top-0 items-center justify-center text-center w-4 h-4 text-xs font-medium -mt-2 -mr-2 bg-red-500 text-white rounded-full">
                 {{ $inCart }}
               </div>
-            @endif --}}
+            @endif
           @endif
           <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
         </a>
