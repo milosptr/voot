bc1f406a0511d125ae80dde2e331163171f77d45
milospetrovic <milosp@ultimatemovers.net>
2022-12-10 16:59:50 +0000

    bring back count

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -21,10 +21,8 @@ class Cart extends Model
 
     public static function cartNumber($id) {
       $cart =  Cart::where('user_id', $id)->first();
-      Log::info(json_decode($cart->cart, true));
-      // Log::info(json_decode($cart->cart));
-      // if($cart && json_decode($cart->cart))
-      //   return count(json_decode($cart->cart));
+      if($cart && json_decode($cart->cart))
+        return count(json_decode($cart->cart, true));
       return 0;
     }
 
