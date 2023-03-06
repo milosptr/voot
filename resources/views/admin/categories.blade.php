d83bca78bb6a1e4ce6b687f444e280bfc4b58238
milospetrovic <63fbbb3ec6e77744a1dc0ed0@bots.bitbucket.org>
2023-03-06 08:48:08 +0000

    add new categories overview, improve reordering

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -7,28 +7,6 @@
 @endsection
 
 @section('content')
-  <div class="min-w-full bg-gray-50">
-    <div class="flex justify-between items-center">
-      <div class="w-32 text-center px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
-        -
-      </div>
-      <div class="w-20 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Image
-      </div>
-      <div class="w-1/3 mr-auto py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Name / Slug
-      </div>
-      <div class="w-24 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Status
-      </div>
-      <div class="w-32 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Products
-      </div>
-      <div class="w-32 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Actions
-      </div>
-    </div>
-  </div>
   <div id="adminCategories"></div>
   @include('components.category.delete')
 @endsection
