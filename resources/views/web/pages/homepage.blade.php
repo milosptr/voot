06cd35f6f8e1ed5a9ed6b331460c6797e2144754
milospetrovic <63fbbb3ec6e77744a1dc0ed0@bots.bitbucket.org>
2023-03-06 20:32:30 +0000

    remove quote from homepage

--- resources/views/web/pages/homepage.blade.php
+++ resources/views/web/pages/homepage.blade.php
@@ -111,24 +111,6 @@
     </div>
    </section>
 
-  <section class="mt-10 mb-32">
-    <div class="container mx-auto relative">
-      <div class="bg-gray-100 bg-opacity-80 rounded-md flex flex-col justify-center items-center py-12 px-8 sm:px-20">
-        <div class="w-full sm:w-3/4">
-          <div class="flex justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#52525b" viewBox="0 0 24 24"><path d="M4.908 17.137c1.504-2.31 1.779-4.45 1.681-5.688-6.132.101-5.696-6.449-1.39-6.449 1.83 0 3.801 1.338 3.801 4.275 0 2.724-1.412 5.845-4.092 7.862zm13 0c1.504-2.31 1.779-4.45 1.681-5.688-6.132.101-5.696-6.449-1.39-6.449 1.83 0 3.801 1.338 3.801 4.275 0 2.724-1.412 5.845-4.092 7.862zm-16.908 3.863c6.108-1.206 10-6.584 10-11.725 0-3.97-2.786-6.275-5.801-6.275-2.615 0-5.199 1.797-5.199 4.979 0 2.601 1.905 4.757 4.396 5.149-.217 2.004-2.165 4.911-4.38 5.746l.984 2.126zm13 0c6.108-1.206 10-6.584 10-11.725 0-3.97-2.786-6.275-5.801-6.275-2.615 0-5.199 1.797-5.199 4.979 0 2.601 1.905 4.757 4.396 5.149-.217 2.004-2.165 4.911-4.38 5.746l.984 2.126z"/></svg>
-          </div>
-          <p class="text-center my-10 font-light text-2xl sm:text-3xl text-gray-600">
-            {{ __('default.quote') }}
-          </p>
-          <div class="flex justify-center">
-            <div class="text-primary-light text-xs sm:text-base tracking-wide uppercase">{{ __('default.quote_author') }}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
   <section class="my-32">
     <div class="container mx-auto relative">
       <div class="flex flex-col sm:flex-row items-center justify-center gap-10">
