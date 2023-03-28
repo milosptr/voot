c05b9211a86a21cb41bf92433766ff8a93954456
milospetrovic <63fbbb3ec6e77744a1dc0ed0@bots.bitbucket.org>
2023-03-28 18:13:14 +0000

    add new host ip

--- app/Services/LisaAxService.php
+++ app/Services/LisaAxService.php
@@ -6,65 +6,68 @@ use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 
-class LisaAxService {
-  public $user;
-  public $requestURL;
-  public $requestType;
-  public $body;
+class LisaAxService
+{
+    public $user;
+    public $requestURL;
+    public $requestType;
+    public $body;
 
-  public function __construct()
-  {
-    // test port 1456
-    // live port 1458
-    $this->requestURL = 'http://213.167.137.207:1458/LisaAxServices.asmx';
-    $this->requestType = 'POST';
+    public function __construct()
+    {
+        // test port 1456
+        // live port 1458
+        $this->requestURL = 'http://213.167.137.207:1458/LisaAxServices.asmx';
+        $this->requestType = 'POST';
 
-    return $this;
-  }
-
-  public static function forCustomer(User $user)
-  {
-    return new Static($user);
-  }
-
-  public static function storeOrderId($response, Order $order)
-  {
-    $clean_xml = str_ireplace(['SOAP-ENV:', 'SOAP:'], '', $response);
-    $xml = simplexml_load_string($clean_xml);
-    $orderId = null;
-    if(isset($xml->Body) && isset($xml->Body->CreateSalesOrderResponse) && isset($xml->Body->CreateSalesOrderResponse->CreateSalesOrderResult) && isset($xml->Body->CreateSalesOrderResponse->CreateSalesOrderResult->SalesOrdID)) {
-      $orderId = (string) $xml->Body->CreateSalesOrderResponse->CreateSalesOrderResult->SalesOrdID;
-      if($orderId !== 'Advania.Axapta.Portal.Entities.AxContainer')
-        $order->update(['order_id' => $orderId]);
+        return $this;
     }
 
-    return $orderId;
-  }
+    public static function forCustomer(User $user)
+    {
+        return new static($user);
+    }
 
-  public function setRequestURL($url)
-  {
-    $this->requestURL = $url;
-    return $this;
-  }
+    public static function storeOrderId($response, Order $order)
+    {
+        $clean_xml = str_ireplace(['SOAP-ENV:', 'SOAP:'], '', $response);
+        $xml = simplexml_load_string($clean_xml);
+        $orderId = null;
+        if (isset($xml->Body) && isset($xml->Body->CreateSalesOrderResponse) && isset($xml->Body->CreateSalesOrderResponse->CreateSalesOrderResult) && isset($xml->Body->CreateSalesOrderResponse->CreateSalesOrderResult->SalesOrdID)) {
+            $orderId = (string) $xml->Body->CreateSalesOrderResponse->CreateSalesOrderResult->SalesOrdID;
+            if ($orderId !== 'Advania.Axapta.Portal.Entities.AxContainer') {
+                $order->update(['order_id' => $orderId]);
+            }
+        }
 
-  public function setRequestType($requestType)
-  {
-    $this->requestType = $requestType;
-  }
+        return $orderId;
+    }
 
-  public function setRequestBody($body) {
-    $this->body = $body;
-    return $this;
-  }
+    public function setRequestURL($url)
+    {
+        $this->requestURL = $url;
+        return $this;
+    }
 
-  public function setBodyForOrderCreated(Order $order)
-  {
-    $customer = $order->user;
-    $company = User::where('key', $order->customer_key)->where('ssn', $order->user->ssn)->first();
-    $companyName = ($company && isset($company)) ? $company->name : $customer->name;
-    $DlvMethod = $order->shipping_method_code ? $order->shipping_method_code : 'VS';
+    public function setRequestType($requestType)
+    {
+        $this->requestType = $requestType;
+    }
 
-    $this->body = '<soap:Envelope
+    public function setRequestBody($body)
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    public function setBodyForOrderCreated(Order $order)
+    {
+        $customer = $order->user;
+        $company = User::where('key', $order->customer_key)->where('ssn', $order->user->ssn)->first();
+        $companyName = ($company && isset($company)) ? $company->name : $customer->name;
+        $DlvMethod = $order->shipping_method_code ? $order->shipping_method_code : 'VS';
+
+        $this->body = '<soap:Envelope
         xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
         xmlns:tem="http://tempuri.org/">
         <soap:Header/>
@@ -85,22 +88,22 @@ class LisaAxService {
                 <tem:Country>'.$customer->country.'</tem:Country>
                 <tem:ContactName>'.$customer->name.'</tem:ContactName>
                 <tem:ContactPhone>'.$customer->phone.'</tem:ContactPhone>
-                <tem:ContactEmail>'.( isset($customer->invoice_email) ? $customer->invoice_email : $customer->email ).'</tem:ContactEmail>
+                <tem:ContactEmail>'.(isset($customer->invoice_email) ? $customer->invoice_email : $customer->email).'</tem:ContactEmail>
               </tem:DeliveryInfo>
               <tem:SalesOrderLines>
       ';
 
-      foreach ($order->order as $item) {
-        $this->body .= '
+        foreach ($order->order as $item) {
+            $this->body .= '
         <tem:SalesOrderLine>
           <tem:LineID>O'.$order->id.'</tem:LineID>
           <tem:ItemId>'.$item['sku'].'</tem:ItemId>
           <tem:Quantity>'.$item['qty'].'</tem:Quantity>
         </tem:SalesOrderLine>
         ';
-      }
+        }
 
-      $this->body .= '
+        $this->body .= '
               </tem:SalesOrderLines>
             </tem:order>
           </tem:CreateSalesOrder>
@@ -108,38 +111,40 @@ class LisaAxService {
       </soap:Envelope>
     ';
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getBody() {
-    return $this->body;
-  }
+    public function getBody()
+    {
+        return $this->body;
+    }
 
-  public function send() {
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => $this->requestURL,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => '',
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => $this->requestType,
-      CURLOPT_POSTFIELDS => $this->body,
-      CURLOPT_HTTPHEADER => array(
-        'Content-Type: text/xml; charset=utf-8;',
-        'X-Forwarded-For: 212.44.101.122',
-        'Host: 212.44.101.122'
-      ),
-    ));
+    public function send()
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $this->requestURL,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => $this->requestType,
+          CURLOPT_POSTFIELDS => $this->body,
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: text/xml; charset=utf-8;',
+            'X-Forwarded-For: 93.95.229.198',
+            'Host: 93.95.229.198'
+          ),
+        ));
 
-    $response = curl_exec($curl);
-    Log::notice(json_encode(curl_getinfo($curl)));
-    Log::notice($this->body);
-    Log::notice('Response from AX: '.json_encode($response));
-    curl_close($curl);
+        $response = curl_exec($curl);
+        Log::notice(json_encode(curl_getinfo($curl)));
+        Log::notice($this->body);
+        Log::notice('Response from AX: '.json_encode($response));
+        curl_close($curl);
 
-    return $response;
-  }
+        return $response;
+    }
 }
